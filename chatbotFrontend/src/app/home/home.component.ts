1946ba6074373da41f8b6a93fba83233c34940e0
Milo Rodrigues <milo.rodrigues97@gmail.com>
2021-11-01 17:36:03 -0300

    Sugestões funcionais + junção de chat e home em um único componente

--- chatbotFrontend/src/app/home/home.component.ts
+++ chatbotFrontend/src/app/home/home.component.ts
@@ -1,8 +1,6 @@
-import { ChangeDetectorRef, Component, Injector, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, Injector, OnInit } from '@angular/core';
 
-import { ChatComponent } from '../chat/chat.component';
 import { Message } from '../models/message.model'
-
 import { ChatbotService } from '../service/chatbot.service';
 
 @Component({
@@ -15,14 +13,14 @@ export class HomeComponent implements OnInit {
   _chatbotService: ChatbotService;
 
   inputText: any;
-
-  teste = '';
-
-  @ViewChild(ChatComponent) private chatComponent: any;
+  messageArray: Array<Message>;
 
   constructor(injector: Injector,
               private cdr: ChangeDetectorRef) {
-    this._chatbotService = injector.get(ChatbotService);  
+    this._chatbotService = injector.get(ChatbotService);
+
+    let initialMessage = new Message("Bem vindo(a) ao Chatbot do Instituto de Computação UFBA! Em que posso ajudar?", Message.typeAnswer);
+    this.messageArray = [initialMessage];
   }
 
   ngOnInit(): void {
@@ -31,22 +29,39 @@ export class HomeComponent implements OnInit {
   onEnviar(): void {
     let me = this;
 
-    let input = this.inputText;
-    if (this.inputText && this.inputText.trim() != ""){
-      let newMessage = new Message(this.inputText, Message.typeQuestion);
-      this.inputText = '';
-      this.chatComponent.addMessage(newMessage);
-      this.cdr.detectChanges();
+    let input = me.inputText;
+    if (me.inputText && me.inputText.trim() != ""){
+      let newMessage = new Message(me.inputText, Message.typeQuestion);
+      me.inputText = '';
+      me.addMessage(newMessage);
+      me.cdr.detectChanges();
 
       me._chatbotService.postEntrada(input).subscribe(response => { 
         let newMessage = new Message((response as any).resposta, Message.typeAnswer, (response as any).sugestoes);
-        console.log(newMessage);
-        this.chatComponent.addMessage(newMessage);
-        this.cdr.detectChanges();
-        
-        console.log((response as any).sugestoes);
+        me.addMessage(newMessage);
+        me.cdr.detectChanges();
       } );
     }
+  }
+
+  onSuggestion(question: string): void {
+    console.log("askSuggestion " + question);
+    let me = this;
+
+    let newMessage = new Message(question, Message.typeQuestion);
+    me.addMessage(newMessage);
+    me.cdr.detectChanges();
+
+    me._chatbotService.postEntrada(question).subscribe(response => {
+      let newMessage = new Message((response as any).resposta, Message.typeAnswer, (response as any).sugestoes);
+      me.addMessage(newMessage);
+      me.cdr.detectChanges();
+    });
+  }
+
+  addMessage(message: Message): void {
+    this.messageArray = [...this.messageArray, message];
+    this.cdr.detectChanges();
   }
 
 }
